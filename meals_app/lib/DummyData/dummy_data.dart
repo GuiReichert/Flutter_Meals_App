a916eed7201784af374c8b18a617d0fb15b19523
Guilherme Reichert <guireichert.tr@gmail.com>
2025-11-10 23:42:23 -0300

    Initial Setup

---
+++ meals_app/lib/DummyData/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category_model.dart';
+
+var availableCategories = [
+  CategoryModel(
+    id: 'c1',
+    name: 'Italian',
+    color: Colors.purple,
+  ),
+  CategoryModel(
+    id: 'c2',
+    name: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  CategoryModel(
+    id: 'c3',
+    name: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  CategoryModel(
+    id: 'c4',
+    name: 'German',
+    color: Colors.amber,
+  ),
+  CategoryModel(
+    id: 'c5',
+    name: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  CategoryModel(
+    id: 'c6',
+    name: 'Exotic',
+    color: Colors.green,
+  ),
+  CategoryModel(
+    id: 'c7',
+    name: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  CategoryModel(
+    id: 'c8',
+    name: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  CategoryModel(
+    id: 'c9',
+    name: 'French',
+    color: Colors.pink,
+  ),
+  CategoryModel(
+    id: 'c10',
+    name: 'Summer',
+    color: Colors.teal,
+  ),
+];
